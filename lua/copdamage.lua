3df722022393eaaafa81e620735a8bf5f8d90613
vojin154 <38527585+vojin154@users.noreply.github.com>
2024-03-14 19:06:14 +0100

    Add files via upload

--- lua/copdamage.lua
+++ lua/copdamage.lua
@@ -1,3 +1,15 @@
+--JUST TO BE SURE IT GETS PROPERLY REMOVED
+
+local function remove(self)
+	if not alive(self._unit) or self._dead then
+		self._unit:contour():remove("highlight_character", true)
+	end
+end
+
+Hooks:PreHook(CopDamage, "clbk_suppression_decay", "clbk_suppression_decay_remove_contour", function(self)
+	remove(self)
+end)
+
 Hooks:PreHook(CopDamage, "die", "die_remove_contour", function(self, attack_data)
-	self._unit:contour():remove("highlight_character", true)
+	remove(self)
 end )
